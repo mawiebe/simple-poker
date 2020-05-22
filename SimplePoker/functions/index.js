24dcca66c6b095ac024400d321d3e7a906269113
Evgeny Tsurinov <etsurinov@gmail.com>
2020-05-22 13:54:55 -0400

    Test cloud functions

--- SimplePoker/functions/index.js
+++ SimplePoker/functions/index.js
@@ -3,6 +3,14 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onCall((data, context) => {
+ // Message text passed from the client.
+const name = data.name;
+// Authentication / user information is automatically added to the request.
+// const uid = context.auth.uid;
+// const name = context.auth.token.name || null;
+// const picture = context.auth.token.picture || null;
+// const email = context.auth.token.email || null;
+
+return {text:"Hello  " + name};
+});
