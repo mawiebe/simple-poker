32325e3d7d61f297a5518fcfd54e28cf4bd0f60a
Evgeny Tsurinov <etsurinov@gmail.com>
2020-05-25 01:30:51 -0400

    Started working on createNextRound

--- SimplePoker/functions/index.js
+++ SimplePoker/functions/index.js
@@ -47,6 +47,39 @@ exports.createGame = functions.https.onCall((data, context) => {
     admin.database().ref(shared.playersGamePath(uid, gid)).set({
       myPosition: 0
     });
+    admin.database().ref(shared.userInfoPath(uid)).update({
+      currentGame: gid
+    });
+
     resolve({gid: gid});
   });
 });
+
+// exports.createNextRound = functions.https.onCall((data, context) => {
+//   if (!context.auth || !context.auth.uid) {
+//     return {error: "Could not get user ID"};
+//   }
+//   var previousGame = data.previousGame;
+//   if (!previousGame) {
+//     return {error: "Wrong previous game ID"};
+//   }
+//
+//   return new Promise(async function(resolve, reject) {
+//     var uid = context.auth.uid;
+//
+//     // Start both request then wait for both to let them run in parallel
+//     var privateGamePromise =
+//       admin.database().ref(shared.privateGamePath(gid)).once('value');
+//     var gamePubInfoPromise =
+//       admin.database().ref(shared.gameInfoPath(gid)).once('value');
+//
+//     var privateSnapshot = await privateGamePromise;
+//     if (!privateSnapshot || !privateSnapshot.val()) {
+//       resolve("Could not find the game (#1)")
+//       return;
+//     }
+//     // Make sure it was one of the players who requested to create the new game
+//     var found = false;
+//     for(var x in privateSnapshot.val().players) {
+//       if (privateSnapshot.val().players[x] == u)
+//     }
