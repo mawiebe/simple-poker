68d5ecc630d1c26284aa82d5767461cb6098a09f
Evgeny Tsurinov <etsurinov@gmail.com>
2020-06-04 02:16:27 -0400

    Finished createNextRound (but did not test)

--- SimplePoker/functions/index.js
+++ SimplePoker/functions/index.js
@@ -3,6 +3,58 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const shared = require('./shared');
+const poker = require('./poker');
+
+async function getUserInfo(uid) {
+  var userInfoSnapshot = await admin.database().ref(
+    shared.userInfoPath(uid)).once('value');
+  if (!userInfoSnapshot || !userInfoSnapshot.val() ||
+      !userInfoSnapshot.val().name) {
+    var error = "Could not find user information. Snapshot is ";
+    if (userInfoSnapshot) {
+      error += "not null. UserInfo is " +
+        JSON.stringify(userInfoSnapshot.val());
+    } else {
+      error += "null";
+    }
+    return {error: error};
+  }
+  return userInfoSnapshot.val();
+}
+
+function checkCorrectGame(public, private){
+  // TODO: implement.
+  // Things to check:
+  //  - number of players
+  //  - only one is is dealer
+  //  - status corresponds to presense of draws.
+  //  - presense of result
+  //  - what else?
+  return {};
+}
+
+
+async function loadGame(gid) {
+  // First send two requests then await both
+  var privateReq =
+   admin.database().ref(shared.privateGamePath(gid)).once('value');
+  var public = await
+   admin.database().ref(shared.gameInfoPath(gid)).once('value');
+  var private = await privateReq;
+
+  if (!public || !private || !public.val() || !private.val()) {
+    return {error: "Could not find game in the database"}
+  }
+  var check = checkCorrectGame(public.val(), private.val());
+  if (check.error) {
+    return check;
+  }
+  return {
+    public: public.val(),
+    private: private.val()
+  }
+}
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,24 +63,17 @@ exports.createGame = functions.https.onCall((data, context) => {
   if (!context.auth || !context.auth.uid) {
     return {error: "Could not get user ID"};
   }
-
+  // Make the rest of the function async to be able to wait for database
+  // data.
   return new Promise(async function(resolve, reject) {
     var uid = context.auth.uid;
-    var userInfoSnapshot = await admin.database().ref(
-      shared.userInfoPath(uid)).once('value');
-    if (!userInfoSnapshot || !userInfoSnapshot.val() ||
-        !userInfoSnapshot.val().name) {
-      var error = "Could not find user information. Snapshot is ";
-      if (userInfoSnapshot) {
-        error += "not null. UserInfo is " +
-          JSON.stringify(userInfoSnapshot.val());
-      } else {
-        error += "null";
-      }
-      resolve({error: error});
+    var userInfoOrError = getUserInfo(uid);
+    if (userInfoOrError.error) {
+      resolve(userInfo);
       return;
     }
-    var playerName = userInfoSnapshot.val().name;
+
+    var playerName = userInfoOrError.name;
 
     var gameRef = admin.database().ref(shared.gamePrivatePath).push({
       players: [uid]
@@ -55,31 +100,59 @@ exports.createGame = functions.https.onCall((data, context) => {
   });
 });
 
-// exports.createNextRound = functions.https.onCall((data, context) => {
-//   if (!context.auth || !context.auth.uid) {
-//     return {error: "Could not get user ID"};
-//   }
-//   var previousGame = data.previousGame;
-//   if (!previousGame) {
-//     return {error: "Wrong previous game ID"};
-//   }
-//
-//   return new Promise(async function(resolve, reject) {
-//     var uid = context.auth.uid;
-//
-//     // Start both request then wait for both to let them run in parallel
-//     var privateGamePromise =
-//       admin.database().ref(shared.privateGamePath(gid)).once('value');
-//     var gamePubInfoPromise =
-//       admin.database().ref(shared.gameInfoPath(gid)).once('value');
-//
-//     var privateSnapshot = await privateGamePromise;
-//     if (!privateSnapshot || !privateSnapshot.val()) {
-//       resolve("Could not find the game (#1)")
-//       return;
-//     }
-//     // Make sure it was one of the players who requested to create the new game
-//     var found = false;
-//     for(var x in privateSnapshot.val().players) {
-//       if (privateSnapshot.val().players[x] == u)
-//     }
+exports.createNextRound = functions.https.onCall((data, context) => {
+  if (!context.auth || !context.auth.uid) {
+    return {error: "Could not get user ID"};
+  }
+  var previousGame = data.previousGame;
+  if (!previousGame) {
+    return {error: "Wrong previous game ID"};
+  }
+
+  return new Promise(async function(resolve, reject) {
+    var uid = context.auth.uid;
+    var oldGame = loadGame(previousGame);
+    if (oldGame.error) {
+      resolve(oldGame);
+    }
+    if (oldGame.public.status != GameState.Showdown) {
+      resolve({error: "Can not play next round of unfinished game"});
+    }
+    // TODO: check current user was in the game.
+    var deck = poker.shuffleDeck();
+    var gameRef = admin.database().ref(shared.gamePrivatePath).push({
+      players: oldGame.private.players,
+      cards: {deck: deck}
+    });
+    var gid = gameRef.key;
+    var gamePub = {
+      status: GameState.WaitingForTurn;
+      players: []
+    }
+    var isDealer =
+      oldGame.public.players[oldGame.public.players.length-1].isDealer;
+    for (int i = 0; i < oldGame.public.players.length; i++) {
+      var oldPlayer = oldGame.public.players[i];
+      gamePub.players.push({
+        name: oldPlayer.name,
+        isDealer: isDealer,   // Dealer is shifted by one
+        previousResult:oldPlayer.result
+      });
+      isDealer = oldPlayer.isDealer;
+    }
+    admin.database().ref(shared.gameInfoPath(gid)).set(gamePub);
+    for (var i in oldGame.private.players) {
+      var uid = oldGame.private.players[i];
+      admin.database().ref(shared.playersGamePath(uid, gid)).set({
+        myPosition: i,
+        // Theoretically we should pick every 5h card but if cards are
+        // shuffled randomly it does not matter.
+        hand: deck.slice(i*poker.handSize, (i+1)*poker.handSize);
+      });
+      admin.database().ref(shared.userInfoPath(uid)).update({
+        currentGame: gid
+      });
+    }
+
+    resolve({gid: gid});
+  }
---
+++ SimplePoker/functions/poker.js
@@ -0,0 +1,33 @@
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+exports.shuffleDeck = function() {
+  var suitsToUse = 4;
+  var ranksLength = 13;
+  var deckSize = suitsToUse * ranksLength;
+  var cards = [...Array(deckSize).keys()]
+  // for (var i = 0; i < suitsToUse*ranks.length; i++) {
+  //   cards.add[i];
+  // }
+  shuffle(cards);
+  return cards;
+}
+exports.handSize = 5;
